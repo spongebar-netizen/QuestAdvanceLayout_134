b741d88fcc143840794b56877a4e03c640358841
Lalu Fatan <lalufatan25@gmail.com>
2025-10-23 15:58:17 +0700

    merubah sedikit

--- app/src/main/java/com/example/pam3/MainActivity.kt
+++ app/src/main/java/com/example/pam3/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.pam3 // Package Anda sudah benar
+package com.example.pam3
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -7,21 +7,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-// Pastikan import Theme Anda sudah benar, sesuai nama project
+
 import com.example.pam3.ui.theme.PAM3Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // Membungkus aplikasi Anda dengan Tema project (PAM3Theme)
+
             PAM3Theme {
-                // Surface adalah container utama
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    // Memanggil Composable 'ActivitasPertama' yang kita buat
                     ActivitasPertama(modifier = Modifier)
                 }
             }
